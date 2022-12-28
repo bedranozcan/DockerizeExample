5c0183bfa1c0638b32ed791512f8fcfb379728bd
bedranozcan <bedra@IDVLABS>
2022-12-28 10:18:51 +0300

    Add project files.

---
+++ DockerizeExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockerizeExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
